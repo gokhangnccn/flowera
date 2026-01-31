6ba90c9fc7396f5e2fdae4e91faaff130a569ba9
gokhan <gokhangnccn@gmail.com>
2026-01-31 18:22:13 +0300

    init flowera monorepo structure

---
+++ src/test/java/com/example/flowera/FloweraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloweraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
